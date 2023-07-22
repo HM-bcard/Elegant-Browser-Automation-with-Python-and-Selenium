dfdd86193178a36e19e4b0ab0be6a77b99770e92
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2023-07-22 15:14:35 +0200

    Add files via upload

--- tools2.py
+++ tools2.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.expected_conditions import alert_is_present
+from selenium.webdriver.common.by import By
+
 #WebDriver
 browser = webdriver.Chrome()
 
@@ -11,10 +13,16 @@ browser.get("http://techstepacademy.com/training-ground")
 
 #alert = Alert(browser)
 
-print("I have arrived")
-WebDriverWait(browser, 20).until(alert_is_present())
-print("An alert appeared")
+#print("I have arrived")
+#WebDriverWait(browser, 20).until(alert_is_present())
+#print("An alert appeared")
 
 
-sel = browser.find_element(By.ID, 'sell')
+sel = browser.find_element(By.ID, 'sel1')
 my_select = Select(sel)
+[elem.text for elem in my_select.options]
+print(elem.text)
+
+my_select.select_by_visible_text('Battlestar Galactica')
+my_select.select_by_index(0)
+my_select.deselect_by_value('second')
